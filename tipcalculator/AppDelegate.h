f79419075c859cf9e14e2b0628328cd4f2c55ad3
Rose Marie Philip <rosephilip@rosephilip-mbp.local>
2015-01-19 12:04:36 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Rose Marie Philip on 1/19/15.
+//  Copyright (c) 2015 Rose Marie Philip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculatorTests/tipcalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  tipcalculatorTests.m
+//  tipcalculatorTests
+//
+//  Created by Rose Marie Philip on 1/19/15.
+//  Copyright (c) 2015 Rose Marie Philip. All rights reserved.
+//
+
+#import "tipcalculatorTests.h"
+
+@implementation tipcalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in tipcalculatorTests");
+}
+
+@end
